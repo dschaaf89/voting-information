97d6ef373007df209706d654bb39ec0de954a9be
Sean Wilson <paintballsean@gmail.com>
2020-08-13 13:13:30 -0700

    Commit will make function realize number input fixed error not seeing userAge work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   $("#ageInput").submit(function(event) {
-    const userAge = parseInt($("#age1").val());
+    const userAge = parseInt($("#userAge").val());
     event.preventDefault();
     console.log(userAge);
-  
+  });
   if ( userAge >= 18) {
     $('#voting').show();
   } else {
     $('#under-18').show();
   }
-});
+
 });
